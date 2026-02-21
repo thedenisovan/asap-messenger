2436a7189222db77d1c947215a74032efba3def9
Dainis Dilevka <thedenisovan@proton.me>
2026-02-21 09:44:18 +0000

    edit: add boolean flag to display svg inside nav button component

--- client/src/components/common/NavButton.tsx
+++ client/src/components/common/NavButton.tsx
@@ -4,17 +4,45 @@ export default function NavButton({
   toPath,
   content,
   className,
+  isArrowVisible = false,
 }: {
   toPath: string;
   content: string;
   className?: string;
+  isArrowVisible?: boolean;
 }) {
   return (
     <Link
-      className={`text-sm rounded-xl px-4 py-0.75 font-medium ${className}`}
+      className={`${className} px-4 py-0.75 text-sm rounded-full font-medium`}
       to={toPath}
     >
       {content}
+      {isArrowVisible && (
+        <div className='group-hover:translate-x-2 duration-200'>
+          <div className='hidden dark:inline'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24px'
+              viewBox='0 -960 960 960'
+              width='24px'
+              fill='#000'
+            >
+              <path d='m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z' />
+            </svg>
+          </div>
+          <div className='inline dark:hidden'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              height='24px'
+              viewBox='0 -960 960 960'
+              width='24px'
+              fill='#fff'
+            >
+              <path d='m560-240-56-58 142-142H160v-80h486L504-662l56-58 240 240-240 240Z' />
+            </svg>
+          </div>
+        </div>
+      )}
     </Link>
   );
 }
--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -3,9 +3,9 @@ import NavButton from '../common/NavButton';
 
 export default function Header() {
   return (
-    <header className='dark:bg-black dark:text-white px-2 md:px-10 xl:px-0 shadow-xs dark:shadow-gray-800'>
+    <header className='dark:bg-black z-2 transition-colors dark:text-white px-2 md:px-10 xl:px-0 border-b border-gray-200 dark:border-gray-800'>
       <div className='max-w-6xl py-4 m-auto flex items-center justify-between'>
-        <h1 className='text-3xl font-bold'>asap.</h1>
+        <h1 className='text-3xl font-bold cursor-alias'>asap.</h1>
         <div className='flex gap-3'>
           <ThemeButton />
           <NavButton
@@ -14,7 +14,7 @@ export default function Header() {
             content='Sign in'
           />
           <NavButton
-            className='bg-black text-white dark:bg-white dark:text-black'
+            className='bg-black hover:bg-black/85 dark:hover:bg-white/90 transition-colors text-white dark:bg-white dark:text-black'
             toPath='auth/signup'
             content='Sign up'
           />
