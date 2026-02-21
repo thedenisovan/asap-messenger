3dd94547342c84433d3b0854391f81c682dce3ba
Dainis Dilevka <thedenisovan@proton.me>
2026-02-21 13:08:11 +0000

    edit: max w size

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ import NavButton from '../common/NavButton';
 export default function Header() {
   return (
     <header className='dark:bg-black z-2 transition-colors dark:text-white px-2 md:px-10 xl:px-0 border-b border-gray-200 dark:border-gray-800'>
-      <div className='max-w-6xl py-4 m-auto flex items-center justify-between'>
+      <div className='max-w-420 py-4 m-auto flex items-center justify-between'>
         <h1 className='text-3xl font-bold cursor-alias'>asap.</h1>
         <div className='flex gap-3'>
           <ThemeButton />
