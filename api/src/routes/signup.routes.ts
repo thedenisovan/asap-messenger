5e3df2b3bf2f8c848f07d993ef47472b685c9450
Dainis Dilevka <thedenisovan@proton.me>
2026-02-28 15:22:56 +0000

    edit: made validator result mw to work for both signin and signup

--- api/src/routes/signup.routes.ts
+++ api/src/routes/signup.routes.ts
@@ -1,10 +1,10 @@
 import { Router, Request, Response } from 'express';
 import signupValidator from '../validators/signup.validators.js';
-import signupValidatorResult from '../validators/signup.validationResult.js';
+import validatorResult from '../validators/validationResult.js';
 import signupUser from '../controllers/auth/signupUser.js';
 
 const signupRoute = Router();
 
-signupRoute.post('/', signupValidator, signupValidatorResult, signupUser);
+signupRoute.post('/', signupValidator, validatorResult, signupUser);
 
 export default signupRoute;
