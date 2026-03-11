e848beb78c9b833e90ebb338231bdfe81114ac68
Dainis Dilevka <thedenisovan@proton.me>
2026-03-11 19:14:35 +0000

    add: case to when data fetch returns err navigate user to err page

--- client/src/pages/chats/sections/ProfileHeader.tsx
+++ client/src/pages/chats/sections/ProfileHeader.tsx
@@ -3,6 +3,9 @@ import useFetchData from '../../../hooks/useFetchData';
 import isOnlineUpdate from '../../../services/api/isOnlineUpdate';
 import exports from '../../../utils/imports';
 import ProfileDropdown from './ProfileDropdown';
+import LightIcon from '../../../components/common/LightIcon';
+import DarkIcon from '../../../components/common/DarkIcon';
+import { useNavigate } from 'react-router';
 
 export default function ProfileHeader({
   isHidden,
@@ -14,7 +17,7 @@ export default function ProfileHeader({
   const { isLoading, serverError, apiData } = useFetchData(
     `chatPage/${localStorage.getItem('uid')}`,
   );
-
+  const navigate = useNavigate();
   useEffect(() => {
     // Init is online update after user signs in ad uid
     // variable updates in local storage
@@ -30,72 +33,67 @@ export default function ProfileHeader({
     return () => clearInterval(interval);
   }, []);
 
-  return (
-    <header>
-      <div
-        style={{
-          justifyContent: isLoading ? '' : 'space-between',
-          gap: isLoading ? '1rem' : '',
-        }}
-        className={`flex p-5`}
-      >
-        {serverError !== null && <h1>ERROR</h1>}
-        {isLoading && !apiData?.profile ? (
-          <>
-            <div className='animate-pulse transition-none duration-900 rounded-full bg-black/70 dark:bg-white/20 h-13 w-13'></div>
-            <div className='flex flex-col animate-pulse duration-900'>
-              <p className='h-5 rounded-2xl w-50 mb-2  bg-black/70 dark:bg-white/20'></p>
-              <p className='h-5 rounded-2xl w-70  bg-black/70 dark:bg-white/20'></p>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className='flex gap-2 items-center'>
-              <div className='bg-black dark:bg-white w-fit p-2 rounded-full'>
-                <img
-                  className='dark:hidden! block!'
-                  width={25}
-                  src={exports.whiteUser}
-                  alt='default profile image'
-                />
-                <img
-                  className='hidden! dark:block!'
-                  width={25}
-                  src={exports.blackUser}
-                  alt='default profile image'
-                />
+  if (serverError !== null) {
+    navigate('/err');
+  } else
+    return (
+      <header>
+        <div
+          style={{
+            justifyContent: isLoading ? '' : 'space-between',
+            gap: isLoading ? '1rem' : '',
+          }}
+          className={`flex p-5`}
+        >
+          {isLoading && !apiData?.profile ? (
+            <>
+              <div className='animate-pulse transition-none duration-900 rounded-full bg-black/70 dark:bg-white/20 h-13 w-13'></div>
+              <div className='flex flex-col animate-pulse duration-900'>
+                <p className='h-5 rounded-2xl w-50 mb-2  bg-black/70 dark:bg-white/20'></p>
+                <p className='h-5 rounded-2xl w-70  bg-black/70 dark:bg-white/20'></p>
               </div>
-              <div>
-                <h1 className='text-lg font-bold'>
-                  {apiData?.profile?.username}
-                </h1>
-                {/* User all ways sees his status as online */}
-                <p className='text-gray-600 dark:text-gray-200'>online</p>
+            </>
+          ) : (
+            <>
+              <div className='flex gap-2 items-center'>
+                <div className='bg-black dark:bg-white w-fit p-2 rounded-full'>
+                  <img
+                    className='dark:hidden! block!'
+                    width={25}
+                    src={exports.whiteUser}
+                    alt='default profile image'
+                  />
+                  <img
+                    className='hidden! dark:block!'
+                    width={25}
+                    src={exports.blackUser}
+                    alt='default profile image'
+                  />
+                </div>
+                <div>
+                  <h1 className='text-lg font-bold'>
+                    {apiData?.profile?.username}
+                  </h1>
+                  {/* User all ways sees his status as online */}
+                  <p className='text-gray-600 dark:text-gray-200'>online</p>
+                </div>
               </div>
-            </div>
-            <div
-              onClick={() => {
-                setIsHidden((val) => !val);
-              }}
-              className='flex items-center relative'
-            >
-              <img
-                className='dark:hidden! block!'
-                width={25}
-                src={exports.moreBlack}
-                alt='default profile image'
-              />
-              <img
-                className='hidden! dark:block!'
-                width={25}
-                src={exports.moreWhite}
-                alt='default profile image'
-              />
-              <ProfileDropdown isHidden={isHidden} />
-            </div>
-          </>
-        )}
-      </div>
-    </header>
-  );
+              <div
+                onClick={() => {
+                  setIsHidden((val) => !val);
+                }}
+                className='flex items-center relative'
+              >
+                <div className='dark:hover:bg-white/10 hover:bg-black/5 p-2 cursor-pointer transition-colors rounded-full'>
+                  <DarkIcon path='M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z' />
+                  <LightIcon path='M480-160q-33 0-56.5-23.5T400-240q0-33 23.5-56.5T480-320q33 0 56.5 23.5T560-240q0 33-23.5 56.5T480-160Zm0-240q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm0-240q-33 0-56.5-23.5T400-720q0-33 23.5-56.5T480-800q33 0 56.5 23.5T560-720q0 33-23.5 56.5T480-640Z' />
+                </div>
+
+                <ProfileDropdown isHidden={isHidden} />
+              </div>
+            </>
+          )}
+        </div>
+      </header>
+    );
 }
