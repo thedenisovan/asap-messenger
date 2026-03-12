ae883ed740b17121293266e7544d884145907028
Dainis Dilevka <thedenisovan@proton.me>
2026-03-12 16:16:32 +0000

    fix: server error 500 bug

---
+++ client/src/services/api/addNewContact.ts
@@ -0,0 +1,26 @@
+import URL from '../../constants/constants';
+
+export default async function addNewContact(email: string) {
+  const profileId = localStorage.getItem('uid');
+  const token = localStorage.getItem('token');
+
+  try {
+    const response = await fetch(URL.BASE_URL + 'dashboard/addNewContact', {
+      method: 'PUT',
+      body: JSON.stringify({ profileId, email }),
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (!response.ok) {
+      const text = await response.text();
+      throw new Error(`Error status: ${response.status}, message: ${text}`);
+    }
+    // Because server does not return json don't try to extract result.json()
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
+    else return console.log(`Unknown error : ${error}`);
+  }
+}
