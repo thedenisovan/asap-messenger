1445894635f6c8b80c397026433dc5867d83d49a
Dainis Dilevka <thedenisovan@proton.me>
2026-03-14 12:03:05 +0000

    fix bug which did not fetch data correctly sometimes when custom hook used initial uid which is set to 0

--- client/src/hooks/useFetchData.tsx
+++ client/src/hooks/useFetchData.tsx
@@ -6,37 +6,42 @@ export default function useFetchData(path: string) {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [apiData, setApiData] = useState<ProfileData | null>(null);
   const [serverError, setServerError] = useState<string | null>(null);
+  const uid = localStorage.getItem('uid');
 
   useEffect(() => {
     const token = localStorage.getItem('token');
 
     const fetchData = async () => {
-      setIsLoading(true);
+      // uid is initially set to 0 in local storage
+      // after user signs in ti is changed to new uid(user id)
+      if (uid !== '0') {
+        setIsLoading(true);
 
-      try {
-        const response = await fetch(`${URL.BASE_URL}${path}`, {
-          headers: {
-            'Content-Type': 'application/json',
-            authorization: 'Bearer ' + token,
-          },
-        });
+        try {
+          const response = await fetch(`${URL.BASE_URL}${path}`, {
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: 'Bearer ' + token,
+            },
+          });
 
-        if (!response.ok) {
-          setServerError(`Error exit status ${response.status}`);
-        } else {
-          const result = await response.json();
-          setApiData(result);
+          if (!response.ok) {
+            setServerError(`Error exit status ${response.status}`);
+          } else {
+            const result = await response.json();
+            setApiData(result);
+          }
+
+          setIsLoading(false);
+        } catch (error) {
+          if (error instanceof Error) setServerError(String(error.message));
+          else setServerError('Unknown error');
         }
-
-        setIsLoading(false);
-      } catch (error) {
-        if (error instanceof Error) setServerError(String(error.message));
-        else setServerError('Unknown error');
       }
     };
 
     fetchData();
-  }, [path]);
+  }, [path, uid]);
 
   return { isLoading, apiData, serverError };
 }
